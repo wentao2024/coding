de1f336da78be55aee3f286667582b9e28775f10
wentao2024 <wentaoma@umass.edu>
2026-01-10 22:24:25 -0500

    Initial commit

---
+++ 146. LRU Cache.py
@@ -0,0 +1,33 @@
+
+"""
+Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+Implement the LRUCache class:
+
+LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+int get(int key) Return the value of the key if the key exists, otherwise return -1.
+void put(int key, int value) Update the value of the key if the key exists. Otherwise, 
+add the key-value pair to the cache. If the number of keys exceeds the capacity from this 
+operation, evict the least recently used key.
+The functions get and put must each run in O(1) average time complexity.
+
+Example 1:
+Input
+["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+Output
+[null, null, null, 1, null, -1, null, -1, 3, 4]"""
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        
+
+    def get(self, key: int) -> int:
+
+
+        
+
+    def put(self, key: int, value: int) -> None:
+        if x<=num:
+            
---
+++ 3	Longest Substring Without Repeating Characters.py
@@ -0,0 +1,20 @@
+"""Example 1:
+Input: s = "abcabcbb"
+Output: 3"""
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s:
+            return 0
+        
+        res = 0
+        x = {}
+        l = 0
+        for r in range(len(s)):
+            char = s[r]
+            if char in x and x[char]>= l:
+                l = x[char]+1
+            x[char] = r
+            res = max(res, r - l+1)
+            
+        return res
