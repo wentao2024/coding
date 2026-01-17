d8843a0242ada559552b37cc50750c7b41942a83
# <wentaoma@umass.edu>
2026-01-16 21:53:17 -0500

    1.15

--- 25	Reverse Nodes in k-Group.py
+++ 25	Reverse Nodes in k-Group.py
@@ -1,6 +1,4 @@
 
-https://leetcode.com/problems/reverse-nodes-in-k-group/
-
 215. Kth Largest Element in an Array
 
 https://leetcode.com/problems/reverse-nodes-in-k-group/
@@ -20,50 +18,44 @@ Output: 5
 
 
 
-
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-def reverseKGroup(head: ListNode, k: int) -> ListNode:
-    if not head or k == 1:
-        return head
-    
-    # 1. 哨兵节点，方便处理头节点变化的情况
-    dummy = ListNode(0)
-    dummy.next = head
-    prev_group_end = dummy
-    
-    while True:
-        # 2. 找到当前组的结尾
-        kth_node = get_kth(prev_group_end, k)
-        if not kth_node:
-            break
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        dummy = ListNode(0, head)
+        prev_group_end = dummy
         
-        next_group_start = kth_node.next
-        
-        # 3. 反转当前组（需要记录当前组的开头，反转后它会变成结尾）
-        curr = prev_group_end.next
-        prev = next_group_start # 这一步很巧妙，直接让原组头的 next 指向下一组开头
-        
-        for _ in range(k):
-            tmp = curr.next
+        while True:
+            # 1. 找到当前组的结尾
+            group_end = prev_group_end
+            for _ in range(k):
+                group_end = group_end.next
+                if not group_end:
+                    return dummy.next # 剩余不足 k 个，直接结束
+            
+            # 2. 记录下一组的开始，并断开当前组
+            next_group_start = group_end.next
+            group_start = prev_group_end.next
+            group_end.next = None # 这一步能有效防止反转时产生环
+            
+            # 3. 反转当前组，并接回原链表
+            # 反转后，原来的 start 变成 end，原来的 end 变成 start
+            prev_group_end.next = self.reverse(group_start)
+            
+            # 4. 重新连接下一组，并移动 prev_group_end
+            group_start.next = next_group_start
+            prev_group_end = group_start
+            
+    # 辅助函数：标准反转链表（LC 206）
+    def reverse(self, head: ListNode) -> ListNode:
+        prev = None
+        curr = head
+        while curr:
+            nxt = curr.next
             curr.next = prev
             prev = curr
-            curr = tmp
-            
-        # 4. 把反转后的部分接回原链表
-        # 此时 prev 是原 kth_node，即新组的头
-        # tmp_start 是原 curr，即反转后的组尾
-        tmp_start = prev_group_end.next
-        prev_group_end.next = kth_node
-        prev_group_end = tmp_start
-        
-    return dummy.next
-
-def get_kth(curr, k):
-    while curr and k > 0:
-        curr = curr.next
-        k -= 1
-    return curr 
+            curr = nxt
+        return prev
